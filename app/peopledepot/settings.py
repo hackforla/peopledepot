00f53cdadbcf4601832330506eea7e706013d653
Fang Liu <fangyiliu@gmail.com>
2022-01-11 16:02:53 -0800

    Setup postgresql in a container
    Rebuild and run both containers
    docker-compose up --build

--- app/peopledepot/settings.py
+++ app/peopledepot/settings.py
@@ -78,8 +78,13 @@ WSGI_APPLICATION = 'peopledepot.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': os.environ.get('SQL_ENGINE', 'django.db.backends.sqlite3'),
+        'NAME': os.environ.get('SQL_DATABASE', BASE_DIR / 'db.sqlite3'),
+        'USER': os.environ.get('SQL_USER', 'user'),
+        'PASSWORD': os.environ.get('SQL_PASSWORD', 'password'),
+        'HOST': os.environ.get('SQL_HOST', 'localhost'),
+        'PORT': os.environ.get('SQL_PORT', '5432'),
+
     }
 }
 
