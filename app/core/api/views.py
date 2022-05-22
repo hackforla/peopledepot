7c784e8373ccef6519464fbce95938f3b2af1520
Fang Liu <fyliu@users.noreply.github.com>
2022-05-22 15:51:44 -0700

    Create test view

---
+++ app/core/api/views.py
@@ -0,0 +1,20 @@
+from .serializers import UserSerializer
+from rest_framework.generics import GenericAPIView
+from rest_framework.mixins import RetrieveModelMixin
+from rest_framework.permissions import IsAuthenticated
+
+
+class UserProfileAPIView(RetrieveModelMixin, GenericAPIView):
+    serializer_class = UserSerializer
+    permission_classes = (IsAuthenticated,)
+
+    def getObject(self):
+        return self.request.user
+
+    def get(self, request, *args, **kwargs):
+        """
+        User Profile
+
+        Get prifile of current logged in user.
+        """
+        return self.retrieve(request, *args, **kwargs)
