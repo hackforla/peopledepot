76f7cb7de94630612addabdf625eed3e88fbc77b
Fang Liu <fyliu@users.noreply.github.com>
2022-06-08 19:35:28 -0700

    Move scripts into scripts directory

---
+++ scripts/buildrun.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -x
+
+SCRIPT_DIR="$(dirname "${BASH_SOURCE[0]}")"
+# https://codefather.tech/blog/bash-get-script-directory/
+
+# clean, build, and run in background
+docker-compose down -v --remove-orphans
+docker-compose up -d --build
+
+"$SCRIPT_DIR"/migrate.sh
+"$SCRIPT_DIR"/createsuperuser.sh
