666208bd579c969be58c3833314c69d6d6838447
Fang Liu <fyliu@users.noreply.github.com>
2022-07-19 12:58:32 -0700

    Add script to generate ER diagrams

---
+++ scripts/erd.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+set -x
+
+# create entity-relation diagram
+# docker-compose exec web python manage.py graph_models -a > erd.dot
+docker-compose exec web python manage.py graph_models --pydot -a -g -o erd.png
