22ad5ae2211ba9056bc616650c12cf2ac21f435f
Fang Liu <fyliu@users.noreply.github.com>
2022-07-19 13:04:52 -0700

    Convert user to viewset and router

---
+++ app/core/api/urls.py
@@ -0,0 +1,16 @@
+from django.urls import path
+from rest_framework import routers
+
+from .views import (
+    UserProfileAPIView,
+    UserViewSet,
+)
+
+router = routers.SimpleRouter()
+router.register(r"users", UserViewSet, basename="users")
+
+urlpatterns = [
+    path("me/", UserProfileAPIView.as_view(), name="my_profile"),
+]
+
+urlpatterns += router.urls
