bb4d48b50603a1e930eeff94dd55d08c9abd5ac8
Fang Liu <fyliu@users.noreply.github.com>
2022-08-04 16:07:53 -0700

    Update contributing for createsuperuser.sh

--- scripts/createsuperuser.sh
+++ scripts/createsuperuser.sh
@@ -4,6 +4,20 @@ set -x
 # create a test superuser for loggin into django admin
 # http://localhost:8000/admin/login
 
+run_install()
+{
+    ## Prompt the user
+    read -p "Do you want to install missing libraries? [Y/n]: " -r answer
+    ## Set the default value if no answer was given
+    answer="${answer:-Y}"
+    ## If the answer matches y or Y, install
+    [[ $answer =~ [Yy] ]] && sudo apt-get install "${reqd_pkgs[@]}"
+}
+
+reqd_pkgs=("expect")
+## Run the run_install function if any of the libraries are missing
+dpkg -s "${reqd_pkgs[@]}" >/dev/null 2>&1 || run_install
+
 /usr/bin/expect << EOF
 
 spawn docker-compose exec web python manage.py createsuperuser --username testadmin --email testadmin@email.com
