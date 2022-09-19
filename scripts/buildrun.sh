081d0640408f8f741c787a9db52aa9d028d15779
Fang Liu <fyliu@users.noreply.github.com>
2022-09-19 11:10:33 -0700

    Combine run scripts in to a master script

--- scripts/buildrun.sh
+++ scripts/buildrun.sh
@@ -3,6 +3,16 @@ set -euo pipefail
 IFS=$'\n\t'
 set -x
 
+SCRIPT_DIR="$(dirname "$0")"
+# https://codefather.tech/blog/bash-get-script-directory/
+
 # clean, build, and run in background
-docker-compose down -v --remove-orphans
-docker-compose up -d --build
+# c     Delete docker containers and networks
+# v    *Remove docker volumes
+# o    *Remove docker orphan containers
+# d     Run in background
+# b     Rebuild
+# m     Run migrations
+# s     Create superuser
+# l     Tail logs after run
+"$SCRIPT_DIR"/run.sh -c -o -d -b -m
