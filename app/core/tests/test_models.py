cc5f3a26cc75c6dbbf6f51ceb238c8457a5d26dd
AzaniaBG <azaniazbaker@gmail.com>
2022-09-30 11:08:16 -0700

    defined simple test for faq_viewed; removed test for sponsor_partner

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -18,12 +18,9 @@ def test_recurring_event(recurring_event):
     assert str(recurring_event) == "Test Recurring Event"
 
 
-def test_sponsor_partner(sponsor_partner):
-    assert str(sponsor_partner) == "Test Sponsor Partner"
-    
 def test_faq(faq):
     assert str(faq) == "Test Faq"
 
 
-def test_faq_viewed():
-    assert str() == "Test Faq_viewed"
+def test_faq_viewed(faq, project):
+    assert str(f"{faq} viewed in {project}") == f"{faq} viewed in {project}"
