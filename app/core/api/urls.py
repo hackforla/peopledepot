b123f0b7536358d51d1cfcf5c8a2e1861c056235
yoyoyojoe <joeyma@gmail.com>
2023-05-13 00:31:29 -0700

    add route for permission-types

--- app/core/api/urls.py
+++ app/core/api/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from rest_framework import routers
 
 from .views import FaqViewedViewSet
+from .views import FaqViewSet
 from .views import ProjectViewSet
 from .views import RecurringEventViewSet
 from .views import SkillViewSet
@@ -18,10 +19,6 @@ router.register(r"practice-areas", PracticeAreaViewSet, basename="practice-area"
 router.register(r"sponsor-partners", SponsorPartnerViewSet, basename="sponsor-partner")
 router.register(r"faqs", FaqViewSet, basename="faq")
 router.register(r"faqs-viewed", FaqViewedViewSet, basename="faq-viewed")
-router.register(r"locations", LocationViewSet, basename="location")
-router.register(r"program-areas", ProgramAreaViewSet, basename="program-area")
-router.register(r"skills", SkillViewSet, basename="skill")
-router.register(r"technologies", TechnologyViewSet, basename="technology")
 
 urlpatterns = [
     path("me/", UserProfileAPIView.as_view(), name="my_profile"),
