c29c9fd391a23e57430a2dcce1e783acd01307b1
Ethan-Strominger <32078396+ethanstrominger@users.noreply.github.com>
2023-05-17 12:19:26 -0400

    Update test_models.py

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -18,7 +18,7 @@ def test_recurring_event(recurring_event):
     assert str(recurring_event) == "Test Recurring Event"
 
 
-def test_practice_area(recurring_event):
+def test_practice_area(practice_area):
     assert str(recurring_event) == "Test Practice Area"
 
 
