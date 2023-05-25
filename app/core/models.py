d5042ebaaa390c1f3d91e0a5dbe1ea3e9076678a
Brenda De Leon <107655225+brdeleon@users.noreply.github.com>
2023-05-25 16:18:36 -0700

    draft for issue 39, not tested

--- app/core/models.py
+++ app/core/models.py
@@ -233,3 +233,20 @@ class Location(AbstractBaseModel):
 
     def __str__(self):
         return f"{self.name}"
+    
+
+class Technology(AbstractBaseModel):
+    """
+    Dictionary of technologies used in projects
+    """
+
+    name = models.CharField(max_length=255)
+    description = models.TextField(blank=True)
+    url = models.URLField(blank=True)
+    logo = models.URLField(blank=True) 
+    active = models.BooleanField(null=True)
+
+    # PK of this model is the ForeignKey for project_partner_xref
+
+    def __str__(self):
+        return f"{self.name}"
