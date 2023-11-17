0b1f062449d169d853fde70788789ca429644d97
Ethan Strominger <ethanstrominger2@gmail.com>
2023-11-16 20:48:55 -0500

    Combine scripts

--- app/data/migrations/0001_programarea_seed.py
+++ app/data/migrations/0001_programarea_seed.py
@@ -1,5 +1,27 @@
 from django.db import migrations
-from core.scripts.programarea_seed import run
+
+from core.models import ProgramArea
+
+
+def run(__code__, __reverse_code__):
+    status = ProgramArea(uuid=1, name="Citizen Engagement")
+    status.save()
+    status = ProgramArea(uuid=2, name="Civic Tech Infrastructure")
+    status.save()
+    status = ProgramArea(uuid=3, name="Diversity / Equity and Inclusion")
+    status.save()
+    status = ProgramArea(uuid=4, name="Environment")
+    status.save()
+    status = ProgramArea(uuid=5, name="Justice")
+    status.save()
+    status = ProgramArea(uuid=6, name="Social Safety Net")
+    status.save()
+    status = ProgramArea(uuid=7, name="Vote / Representation")
+    status.save()
+    status = ProgramArea(uuid=8, name="Workforce Development")
+    status.save()
+    status = ProgramArea(uuid=9, name="Community of Practice")
+    status.save()
 
 
 class Migration(migrations.Migration):
--- app/data/migrations/0002_practicearea_seed.py
+++ app/data/migrations/0002_practicearea_seed.py
@@ -1,5 +1,16 @@
 from django.db import migrations
-from core.scripts.practicearea_seed import run
+from core.models import PracticeArea
+
+
+def run(__code__, __reverse_code__):
+    status = PracticeArea(uuid=1, name="Development")
+    status.save()
+    status = PracticeArea(uuid=2, name="Project Management")
+    status.save()
+    status = PracticeArea(uuid=3, name="Design")
+    status.save()
+    status = PracticeArea(uuid=4, name="Professional Development")
+    status.save()
 
 
 class Migration(migrations.Migration):
