49f1bb89b7094745e158cf596d8f4eee1fddea6d
Ethan Strominger <ethanstrominger2@gmail.com>
2023-12-30 13:21:08 -0500

    Customize login

--- app/peopledepot/urls.py
+++ app/peopledepot/urls.py
@@ -4,6 +4,7 @@ from django.urls import path
 from drf_spectacular.views import SpectacularAPIView
 from drf_spectacular.views import SpectacularRedocView
 from drf_spectacular.views import SpectacularSwaggerView
+from django.views.generic.base import TemplateView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
@@ -19,6 +20,7 @@ urlpatterns = [
         SpectacularRedocView.as_view(url_name="schema"),
         name="redoc",
     ),
+    path('no_admin_access/', TemplateView.as_view(template_name='admin/no_admin_access.html'), name='no_admin_access'),
     path('accounts/', include('allauth.urls')),
-    # path('admin/', include('allauth.urls')),
+    path('admin/', include('allauth.urls')),
 ]
