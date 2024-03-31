29eac6882231483031dbb172b88d558888171d1a
AzaniaBG <azaniazbaker@gmail.com>
2024-03-31 09:34:29 -0700

    Rename technology table to stack_element and add element_type FK
    This deletes and recreates the table as new, which is not ideal, but acceptable
    since we're still in development and not losing data from the migration. It's
    possible to rename and add FK in separate migrations but it would take 3 of
    them.

--- app/core/tests/conftest.py
+++ app/core/tests/conftest.py
@@ -14,8 +14,8 @@ from ..models import ProgramArea
 from ..models import Project
 from ..models import Sdg
 from ..models import Skill
+from ..models import StackElement
 from ..models import StackElementType
-from ..models import Technology
 
 
 @pytest.fixture
@@ -145,8 +145,10 @@ def skill():
 
 
 @pytest.fixture
-def technology():
-    return Technology.objects.create(name="Test Technology")
+def stack_element(stack_element_type):
+    return StackElement.objects.create(
+        name="Test Stack Element", element_type=stack_element_type
+    )
 
 
 @pytest.fixture
