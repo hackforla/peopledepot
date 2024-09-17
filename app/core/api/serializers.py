1419b643c288db23c733f97a77c12c5fa0901f60
Ethan Strominger <ethanstrominger2@gmail.com>
2024-09-17 16:29:32 -0400

    Make changes based on Fang's review

--- app/core/api/serializers.py
+++ app/core/api/serializers.py
@@ -53,6 +53,11 @@ class UserPermissionSerializer(serializers.ModelSerializer):
             "project",
             "practice_area",
         )
+        read_only_fields = (
+            "uuid",
+            "created_at",
+            "updated_at",
+        )
 
 
 class UserSerializer(serializers.ModelSerializer):
