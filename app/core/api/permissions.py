6878b08d9c921751fe3835a90c6ebd254ac613fc
Ethan Strominger <ethanstrominger2@gmail.com>
2024-10-12 22:39:36 -0400

    Refactor

--- app/core/api/permissions.py
+++ app/core/api/permissions.py
@@ -9,23 +9,13 @@ class DenyAny(BasePermission):
         return False
 
 
-class CheckUserPermission:
-    @staticmethod
-    def validate_post(request):
-        if "time_zone" not in request.data:
-            request.data["time_zone"] = "America/Los_Angeles"
-        PermissionCheck.validate_user_fields_postable(request.user, request.data)
-
-    @staticmethod
-    def validate_patch(request, obj):
-        PermissionCheck.validate_user_fields_patchable(request.user, obj, request.data)
-
-
 class UserPermissionCheck(BasePermission):
 
     def has_permission(self, request, __view__):
         if request.method == "POST":
-            CheckUserPermission.validate_post(request)
+            if "time_zone" not in request.data:
+                request.data["time_zone"] = "America/Los_Angeles"
+            PermissionCheck.validate_user_fields_postable(request.user, request.data)
         return True  # Default to allow the request
 
     def has_object_permission(self, request, view, obj):
