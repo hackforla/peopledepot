852504aaef4f7faffd63347704f457c20691c319
Ethan Strominger <ethanstrominger2@gmail.com>
2024-10-30 18:17:27 -0400

    Refactor, fix test_patch_user.py

--- app/core/tests/test_patch_users.py
+++ app/core/tests/test_patch_users.py
@@ -1,85 +1,66 @@
 import pytest
 from django.urls import reverse
 from rest_framework import status
-from rest_framework.test import APIRequestFactory
-from rest_framework.test import force_authenticate
+from rest_framework.test import APIClient
 
-from core.api.views import UserViewSet
-from core.tests.utils.seed_constants import garry_name, wanda_admin_project
+
+from core.api.permission_check import FieldPermissionCheck
+from core.models import User
+from core.tests.utils.seed_constants import garry_name
+from core.tests.utils.seed_constants import valerie_name
+
 from core.tests.utils.seed_user import SeedUser
+from unittest.mock import patch
+
+count_website_members = 4
+count_people_depot_members = 3
+count_members_either = 6
 
 
 @pytest.mark.django_db
 @pytest.mark.load_user_data_required  # see load_user_data_required in conftest.py
 class TestPatchUser:
-    @staticmethod
-    def _patch_request_to_viewset(requester, patch_data, target_user):
-        new_data = patch_data.copy()
-        factory = APIRequestFactory()
-        request = factory.patch(reverse("user-detail"), data=new_data, format="json")
-        force_authenticate(request, user=requester)
-        view = UserViewSet.as_view({"patch": "partial_update"})
-        response = view(request)
-        return response
-    
-    @classmethod
-    def test_valid_patch(cls):
-        """Test PATCH request returns success when the request fields match configured fields.
 
-        This test mocks a PATCH request to skip submitting the request to the server and instead
-        calls the view directly with the request.  This is done so that variables used by the
-        server can be set to test values.
-        """
-        requester = SeedUser.get_user(garry_name)  # project lead for website
+    @patch.object(FieldPermissionCheck, "validate_user_related_request")
+    def test_patch_request_calls_validate_request(self, mock_validate_user_related_request):
+        """Test that the patch requests succeeds when the requester is an admin."""
+        requester = SeedUser.get_user(garry_name)
+        client = APIClient()
+        client.force_authenticate(user=requester)
 
-        create_data = {
-            "username": "foo",
-            "last_name": "Smith",
-            "first_name": "John",
-            "gmail": "smith@example.com",
-            "time_zone": "America/Los_Angeles",
-            "password": "password",
-            "first_name": "John",
+        target_user = SeedUser.get_user(valerie_name)
+        url = reverse("user-detail", args=[target_user.uuid])
+        data = {
+            "last_name": "Updated",
+            "gmail": "update@example.com",
         }
-        response = cls._patch_request_to_viewset(requester, create_data)
-        assert response.status_code == status.HTTP_201_CREATED
+        client.patch(url, data, format="json")
+        __args__, kwargs = mock_validate_user_related_request.call_args
+        request_received = kwargs.get("request")
+        target_user_received = kwargs.get("target_user")
+        assert request_received.data == data
+        assert request_received.user == requester
+        assert target_user_received == target_user
+        # assert (
+        #     response.status_code == status.HTTP_200_OK
+        # ), f"API Error: {response.status_code} - {response.content.decode()}"
+        # assert len(response.data) == len(User.object.all())
 
-    def test_patch_with_not_allowed_fields(cls):
-        """Test patch request returns 400 response when request fields do not match configured fields.
-
-        Fields are configured to not include last_name.  The test will attempt to create a user
-        with last_name in the request data.  The test should fail with a 400 status code.
-
-        See documentation for test_allowable_patch_fields_configurable for more information.
+    def test_admin_cannot_patch_created_at(self):
+        """Test that the patch request raises a validation exception
+        when the request fields includes created_date, even if the
+        requester is an admin.
         """
+        requester = SeedUser.get_user(garry_name)
+        client = APIClient()
+        client.force_authenticate(user=requester)
 
-        requester = SeedUser.get_user(garry_name)  # project lead for website
-        patch_data = {
-            "gmail": "smith@example.com",
+        target_user = SeedUser.get_user(valerie_name)
+        url = reverse("user-detail", args=[target_user.uuid])
+        data = {
             "created_at": "2022-01-01T00:00:00Z",
         }
-        response = cls._patch_request_to_viewset(requester, patch_data)
+        response = client.patch(url, data, format="json")
         assert response.status_code == status.HTTP_400_BAD_REQUEST
+        assert "created_at" in response.json()[0]
 
-    def test_patch_with_unprivileged_requester(cls):
-        """Test patch request returns 400 response when request fields do not match configured fields.
-
-        Fields are configured to not include last_name.  The test will attempt to create a user
-        with last_name in the request data.  The test should fail with a 400 status code.
-
-        See documentation for test_allowable_patch_fields_configurable for more information.
-        """
-
-        requester = SeedUser.get_user(wanda_admin_project)  # project lead for website
-        patch_data = {
-            "username": "foo",
-            "first_name": "Mary",
-            "last_name": "Smith",
-            "gmail": "smith@example.com",
-            "time_zone": "America/Los_Angeles",
-            "password": "password",
-            "first_name": "John",
-            "created_at": "2022-01-01T00:00:00Z",
-        }
-        response = cls._patch_request_to_viewset(requester, patch_data)
-        assert response.status_code == status.HTTP_401_UNAUTHORIZED
