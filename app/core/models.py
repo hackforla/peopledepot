910506f6244dee4619844c25220075b3c8897d82
dmartin4820 <dom4822@yahoo.com>
2024-11-13 23:40:29 -0800

    feat: add model: project_status

--- app/core/models.py
+++ app/core/models.py
@@ -116,6 +116,18 @@ class User(PermissionsMixin, AbstractBaseUser, AbstractBaseModel):
         return f"{self.email}"
 
 
+class ProjectStatus(AbstractBaseModel):
+    """
+    Dictionary of status options for project
+    """
+
+    name = models.CharField(max_length=255)
+    description = models.TextField(blank=True)
+
+    def __str__(self):
+        return f"{self.name}"
+
+
 class Project(AbstractBaseModel):
     """
     List of projects
@@ -137,7 +149,9 @@ class Project(AbstractBaseModel):
 "Authorization: token [gh_PAT]" \
 https://api.github.com/repos/[org]/[repo]',
     )
-    # current_status_id = models.ForeignKey("status", on_delete=models.PROTECT)
+    current_status_id = models.ForeignKey(
+        ProjectStatus, null=True, on_delete=models.PROTECT
+    )
     hide = models.BooleanField(default=True)
     # location_id = models.ForeignKey("location", on_delete=models.PROTECT)
     google_drive_id = models.CharField(max_length=255, blank=True)
