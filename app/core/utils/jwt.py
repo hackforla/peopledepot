6d69f404a7d098514c5209ee9ef9d5a380b39202
Ethan Strominger <ethanstrominger2@gmail.com>
2024-12-01 15:58:20 -0500

    Restore jwt.py

--- app/core/utils/jwt.py
+++ app/core/utils/jwt.py
@@ -1,9 +1,6 @@
-# todo: fix
-# get expired to work
-import traceback
-
 import jwt
 from django.contrib.auth import authenticate
+from jwt import DecodeError
 from jwt.algorithms import RSAAlgorithm
 from rest_framework_jwt.settings import api_settings
 
@@ -21,12 +18,19 @@ def cognito_jwt_decode_handler(token):
     https://aws.amazon.com/premiumsupport/knowledge-center/decode-verify-cognito-json-token/
     Almost the same as default 'rest_framework_jwt.utils.jwt_decode_handler', but 'secret_key' feature is skipped
     """
+    options = {"verify_exp": api_settings.JWT_VERIFY_EXPIRATION}
+    unverified_header = jwt.get_unverified_header(token)
+    if "kid" not in unverified_header:
+        raise DecodeError("Incorrect authentication credentials.")
+
+    kid = unverified_header["kid"]
     try:
-        options = {"verify_exp": api_settings.JWT_VERIFY_EXPIRATION}
-        unverified_header = jwt.get_unverified_header(token)
-        kid = unverified_header["kid"]
         # pick a proper public key according to `kid` from token header
         public_key = RSAAlgorithm.from_jwk(api_settings.JWT_PUBLIC_KEY[kid])
+    except KeyError:
+        # in this place we could refresh cached jwks and try again
+        raise DecodeError("Can't find proper public key in jwks")
+    else:
         return jwt.decode(
             token,
             public_key,
@@ -37,7 +41,3 @@ def cognito_jwt_decode_handler(token):
             issuer=api_settings.JWT_ISSUER,
             leeway=api_settings.JWT_LEEWAY,
         )
-    except Exception as e:
-        print(traceback.format_exc())
-        print("Authentication failed", repr(e))
-        raise e
