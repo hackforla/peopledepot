551c6603f0d6fdc2160ea3b4385be0d56e9c0d29
Ethan Strominger <ethanstrominger2@gmail.com>
2024-12-24 07:26:27 -0500

    Remove extra files

--- app/utils/utils.py
+++
@@ -1,8 +0,0 @@
-def read_file_and_close(file_path):
-    with open(file_path, "r") as f:
-        return f.read()
-
-
-def read_lines_and_close(file_path):
-    with open(file_path, "r") as f:
-        return f.readlines()
