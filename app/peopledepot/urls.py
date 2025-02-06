b7099228ea8b5654a1022c6be2d2650c9c173030
Fang Yi Liu <fangyiliu@gmail.com>
2025-02-06 02:34:28 -0800

    add a simple page at the site root
    The purpose of this is to have a valid page at / to appease the load-
    balancer so it doesn't spam the logs.

--- app/peopledepot/urls.py
+++ app/peopledepot/urls.py
@@ -1,11 +1,18 @@
 from django.contrib import admin
+from django.http import HttpResponse
 from django.urls import include
 from django.urls import path
 from drf_spectacular.views import SpectacularAPIView
 from drf_spectacular.views import SpectacularRedocView
 from drf_spectacular.views import SpectacularSwaggerView
 
+
+def index(request):
+    return HttpResponse("You're at the peopledepot index.")
+
+
 urlpatterns = [
+    path("", index),
     path("admin/", admin.site.urls),
     path("api/v1/", include("core.api.urls")),
     path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
