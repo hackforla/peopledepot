c0e014b4a71e04ccfbad99c51ca8ddeaa4e077e7
Ethan Strominger <ethanstrominger2@gmail.com>
2025-09-19 14:17:30 -0400

    Restore serializers and 0013_reverse_rank changes

--- app/core/api/serializers.py
+++ app/core/api/serializers.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 from timezone_field.rest_framework import TimeZoneSerializerField
 
+from core.api.user_related_request import UserRelatedRequest
 from core.models import Affiliate
 from core.models import Affiliation
 from core.models import CheckType
@@ -15,19 +16,107 @@ from core.models import PracticeArea
 from core.models import ProgramArea
 from core.models import Project
 from core.models import ProjectStatus
-from core.models import ProjectUrl
 from core.models import Referrer
 from core.models import ReferrerType
 from core.models import Sdg
 from core.models import Skill
 from core.models import SocMajor
-from core.models import SocMinor
 from core.models import StackElement
 from core.models import StackElementType
 from core.models import UrlType
 from core.models import User
 from core.models import UserPermission
 from core.models import UserStatusType
+
+# ------------------------
+# Base serializers
+# ------------------------
+
+
+class ReadOnlyBaseSerializer(serializers.ModelSerializer):
+    """Base serializer with common read-only fields."""
+
+    class Meta:
+        abstract = True
+        read_only_fields = ("uuid", "created_at", "updated_at")
+
+
+class BaseUserSerializer(ReadOnlyBaseSerializer):
+    """
+    Base serializer for the User model.
+
+    Includes all commonly needed fields when retrieving user info.
+    Intended to be inherited by other serializers to avoid repetition.
+
+    Attributes:
+        time_zone: Uses TimeZoneSerializerField (without pytz) for user time zones.
+    """
+
+    time_zone = TimeZoneSerializerField(use_pytz=False)
+
+    class Meta(ReadOnlyBaseSerializer.Meta):
+        model = User
+        fields = (
+            "uuid",
+            "username",
+            "created_at",
+            "updated_at",
+            "is_superuser",
+            "is_active",
+            "is_staff",
+            "email",
+            "first_name",
+            "last_name",
+            "email_gmail",
+            "email_preferred",
+            "job_title_current_intake",
+            "job_title_target_intake",
+            "current_skills",
+            "target_skills",
+            "referrer",
+            "linkedin_account",
+            "github_handle",
+            "slack_id",
+            "phone",
+            "texting_ok",
+            "time_zone",
+            "practice_area_primary",
+            "practice_area_secondary",
+            "practice_area_target_intake",
+            "email_cognito",
+            "user_status",
+        )
+
+
+class UserSerializer(BaseUserSerializer):
+    """
+    Serializer for retrieving full user info with custom representation.
+
+    Overrides `to_representation` to include computed or related fields via
+    UserRelatedRequest.get_serializer_representation.
+    """
+
+    def to_representation(self, instance):
+        representation = super().to_representation(instance)
+        return UserRelatedRequest.get_serializer_representation(
+            self, instance, representation
+        )
+
+
+class UserProfileSerializer(BaseUserSerializer):
+    """
+    Serializer for retrieving basic user profile information.
+
+    Inherits from BaseUserSerializer without modifying to_representation.
+    Use for endpoints where standard model fields suffice.
+    """
+
+    pass
+
+
+# ------------------------
+# Other serializers
+# ------------------------
 
 
 class PracticeAreaSerializer(serializers.ModelSerializer):
@@ -67,50 +156,6 @@ class UserPermissionSerializer(serializers.ModelSerializer):
             "uuid",
             "created_at",
             "updated_at",
-        )
-
-
-class UserSerializer(serializers.ModelSerializer):
-    """Used to retrieve user info"""
-
-    time_zone = TimeZoneSerializerField(use_pytz=False)
-
-    class Meta:
-        model = User
-        fields = (
-            "uuid",
-            "username",
-            "created_at",
-            "updated_at",
-            "email",
-            "first_name",
-            "last_name",
-            "email_gmail",
-            "email_preferred",
-            "job_title_current_intake",
-            "job_title_target_intake",
-            "current_skills",
-            "target_skills",
-            "referrer",
-            "linkedin_account",
-            "github_handle",
-            "slack_id",
-            "phone",
-            "texting_ok",
-            "time_zone",
-            "practice_area_primary",
-            "practice_area_secondary",
-            "practice_area_target_intake",
-            "email_cognito",
-            "is_active",
-            "user_status",
-        )
-        read_only_fields = (
-            "uuid",
-            "created_at",
-            "updated_at",
-            "username",
-            "email",
         )
 
 
@@ -431,15 +476,6 @@ class SocMajorSerializer(serializers.ModelSerializer):
         read_only_fields = ("uuid", "created_at", "updated_at")
 
 
-class SocMinorSerializer(serializers.ModelSerializer):
-    """Used to retrieve soc_minor info"""
-
-    class Meta:
-        model = SocMinor
-        fields = ("uuid", "soc_major", "occ_code", "title")
-        read_only_fields = ("uuid", "created_at", "updated_at")
-
-
 class UrlTypeSerializer(serializers.ModelSerializer):
     """Used to retrieve url_type info"""
 
@@ -479,19 +515,5 @@ class ReferrerSerializer(serializers.ModelSerializer):
             "referrer_type",
             "contact_name",
             "contact_email",
-        )
-        read_only_fields = ("uuid", "created_at", "updated_at")
-
-
-class ProjectUrlSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = ProjectUrl
-        fields = (
-            "uuid",
-            "project",
-            "url_type",
-            "name",
-            "external_id",
-            "url",
         )
         read_only_fields = ("uuid", "created_at", "updated_at")
---
+++ app/data/migrations/0013_reverse_rank_permissiontype_seed.py
@@ -0,0 +1,25 @@
+from django.db import migrations
+from core.models import PermissionType
+
+
+def reverse_and_multiply_ranks(apps, schema_editor):
+    for pt in PermissionType.objects.all():
+        pt.rank = (9 - pt.rank) * 10
+        pt.save(update_fields=["rank"])
+
+
+def revert_ranks(apps, schema_editor):
+    for pt in PermissionType.objects.all():
+        pt.rank = 9 - (pt.rank // 10)
+        pt.save(update_fields=["rank"])
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ("data", "0012_leadershiptype_seed"),
+    ]
+
+    operations = [
+        migrations.RunPython(reverse_and_multiply_ranks, reverse_code=revert_ranks),
+    ]
