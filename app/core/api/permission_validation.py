8ebf67dfab0e84e2b6d33eced7dc834db9dcda1b
Ethan Strominger <ethanstrominger2@gmail.com>
2025-09-19 14:44:23 -0400

    Move logic to permission_validation.py

---
+++ app/core/api/permission_validation.py
@@ -0,0 +1,207 @@
+import csv
+from pathlib import Path
+from typing import Any
+
+from rest_framework.exceptions import PermissionDenied
+
+from constants import admin_global  # Assuming you have this constant
+from constants import field_permissions_csv_file
+from core.models import PermissionType
+from core.models import UserPermission
+
+
+class PermissionValidation:
+    """A collection of static methods for validating user permissions."""
+
+    @staticmethod
+    def is_admin(user) -> bool:
+        """Check if a user assigned "globalAdmin" permission."""
+        permission_type = PermissionType.objects.filter(name=admin_global).first()
+        # return True
+        return UserPermission.objects.filter(
+            permission_type=permission_type, user=user
+        ).exists()
+
+    @staticmethod
+    def get_rank_dict() -> dict[str, int]:
+        """Return a dictionary mapping permission names to their ranks.
+        Example: {"adminGlobal": 1, "adminProject": 2, "practiceLeadProject": 3, "memberProject": 4}.
+        Used in algorithm to determine most privileged permission type between two users.  The lower the rank,
+        the more privileged the permission.
+        """
+        permissions = PermissionType.objects.values("name", "rank")
+        return {perm["name"]: perm["rank"] for perm in permissions}
+
+    @staticmethod
+    def get_csv_field_permissions() -> dict[str, dict[str, list[dict[str, Any]]]]:
+        """Read the field permissions from a CSV file.
+        Returns a list of dictionaries, each representing a row in the CSV file.
+        Each dictionary contains key values for: table_name, field_name, get, post, patch.
+        """
+        file_path = Path(field_permissions_csv_file)
+        with file_path.open() as file:
+            reader = csv.DictReader(file)
+            return list(reader)
+
+    @classmethod
+    def get_permitted_fields(
+        cls, operation: str, permission_type: str, table_name: str
+    ) -> list[str]:
+        """
+        Return the list of field names accessible for a user with the given permission type
+        for a given operation.
+
+        Parameters:
+            operation (str): The type of operation. (e.g., "get", "post", "patch").
+            permission_type (str): The permission type of the requesting user
+                (e.g., "adminGlobal", "adminProject", etc.).
+            table_name (str): The name of the table/model
+                (e.g., "User", "Project").
+
+        Returns:
+            list[str]: A list of field names that the user with the given
+            permission_type can access for the specified operation on the
+            specified table.
+
+        Example:
+            >>> get_fields("get", "adminProject", "User")
+            ["first_name", "last_name"]
+        """
+        if not permission_type:  # Early exit guard clause
+            return []
+
+        valid_fields = set()
+
+        for field_permission in cls.get_csv_field_permissions():
+            if cls.has_field_permission(
+                operation=operation,
+                permission_type=permission_type,
+                table_name=table_name,
+                field=field_permission,
+            ):
+                valid_fields.add(field_permission["field_name"])
+
+        return list(valid_fields)
+
+    @classmethod
+    def get_fields_for_post_request(cls, request, table_name):
+        requesting_user = request.user
+        if not cls.is_admin(requesting_user):
+            raise PermissionDenied("You do not have privilges to create.")
+        fields = cls.get_permitted_fields(
+            operation="post",
+            table_name=table_name,
+            permission_type=admin_global,
+        )
+        return fields
+
+    @classmethod
+    def get_fields_for_patch_request(cls, request, table_name, response_related_user):
+        requesting_user = request.user
+        requesting_user = request.user
+        most_privileged_perm_type = cls.get_most_privileged_perm_type(
+            requesting_user, response_related_user
+        )
+        fields = cls.get_permitted_fields(
+            operation="patch",
+            table_name=table_name,
+            permission_type=most_privileged_perm_type,
+        )
+        return fields
+
+    @classmethod
+    def get_fields_for_response(cls, request, table_name, response_related_user):
+        requesting_user = request.user
+        most_privileged_perm_type = cls.get_most_privileged_perm_type(
+            requesting_user, response_related_user
+        )
+        fields = cls.get_permitted_fields(
+            operation="get",
+            table_name=table_name,
+            permission_type=most_privileged_perm_type,
+        )
+        return fields
+
+    @classmethod
+    def get_most_privileged_perm_type(
+        cls, requesting_user, response_related_user
+    ) -> str:
+        """Return the most privileged permission type between users."""
+        if cls.is_admin(requesting_user):
+            return admin_global
+
+        target_projects = UserPermission.objects.filter(
+            user=response_related_user
+        ).values_list("project__name", flat=True)
+        target_projects = UserPermission.objects.filter(
+            user=response_related_user
+        ).values_list("project__name", flat=True)
+
+        permissions = UserPermission.objects.filter(
+            user=requesting_user, project__name__in=target_projects
+        ).values("permission_type__name", "permission_type__rank")
+
+        if not permissions:
+            return ""
+
+        min_permission = min(permissions, key=lambda p: p["permission_type__rank"])
+        return min_permission["permission_type__name"]
+
+    @classmethod
+    def get_response_fields(cls, request, table_name, response_related_user) -> None:
+        """Ensure the requesting user can patch the provided fields."""
+        requesting_user = request.user
+        most_privileged_perm_type = cls.get_most_privileged_perm_type(
+            requesting_user, response_related_user
+        )
+        fields = cls.get_permitted_fields(
+            operation="get",
+            table_name=table_name,
+            permission_type=most_privileged_perm_type,
+        )
+        return fields
+
+    @classmethod
+    def has_field_permission(
+        cls, operation: str, permission_type: str, table_name: str, field: dict
+    ) -> bool:
+        """
+        Determine whether a user with a given permission type has access to a field
+        for a specific operation on a specific table.
+
+        Parameters:
+            operation (str): The type of operation ("get", "post", or "patch").
+            permission_type (str): The user's permission type
+                (e.g., "adminGlobal", "adminProject").
+            table_name (str): The name of the table/model to check (e.g., "User").
+            field (dict): A dictionary describing the field, including at least:
+                - "field_name"
+                - "table_name"
+                - operation-specific permission values
+                  (e.g., {"get": "adminProject"}).
+
+        Returns:
+            bool: True if the permission type allows access to the field for the operation,
+            False otherwise.
+
+        Example:
+            >>> field_info = {
+            ...     "field_name": "email",
+            ...     "table_name": "User",
+            ...     "get": "adminProject"
+            ... }
+            >>> has_field_permission("get", "adminProject", "User", field_info)
+            True
+        """
+        operation_permission_type = field.get(operation, "")
+        if not operation_permission_type or field.get("table_name") != table_name:
+            return False
+
+        rank_dict = cls.get_rank_dict()
+        if (
+            permission_type not in rank_dict
+            or operation_permission_type not in rank_dict
+        ):
+            return False
+
+        return rank_dict[permission_type] <= rank_dict[operation_permission_type]
