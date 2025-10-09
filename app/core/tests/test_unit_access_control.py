6e53233128013f4b566b16a68a56bea5d04187e3
Ethan Strominger <ethanstrominger2@gmail.com>
2025-10-09 06:52:36 -0400

    Create access control  methods

---
+++ app/core/tests/test_unit_access_control.py
@@ -0,0 +1,118 @@
+from unittest.mock import mock_open
+from unittest.mock import patch
+
+import pytest
+from rest_framework.exceptions import PermissionDenied
+from rest_framework.exceptions import ValidationError
+
+from constants import GLOBAL_ADMIN
+from constants import ADMIN_PROJECT
+from constants import MEMBER_PROJECT
+from constants import PRACTICE_LEAD_PROJECT
+from core.api.access_control import AccessControl
+# from core.api.user_related_request import UserRelatedRequest
+from core.api.views import UserViewSet
+from core.tests.utils.seed_constants import garry_name
+from core.tests.utils.seed_constants import patti_name
+from core.tests.utils.seed_constants import wally_name
+from core.tests.utils.seed_constants import wanda_admin_project
+from core.tests.utils.seed_constants import zani_name
+from core.tests.utils.seed_user import SeedUser
+
+keys = ["table_name", "field_name", "get", "patch", "post"]
+rows = [
+    ["User", "field1", MEMBER_PROJECT, PRACTICE_LEAD_PROJECT, GLOBAL_ADMIN],
+    ["User", "field2", ADMIN_PROJECT, ADMIN_PROJECT, GLOBAL_ADMIN],
+    ["User", "field3", ADMIN_PROJECT, GLOBAL_ADMIN, GLOBAL_ADMIN],
+    ["User", "system_field", MEMBER_PROJECT, "", ""],
+    ["foo", "bar", MEMBER_PROJECT, MEMBER_PROJECT, MEMBER_PROJECT],
+]
+# Create an array of dictionaries with keys specified by keys[] andsss
+# values for each row specified by rows
+mock_data = [dict(zip(keys, row)) for row in rows]
+
+
+class MockSimplifiedRequest:
+    def __init__(self, user, data, method):
+        self.user = user
+        self.data = data
+        self.method = method
+
+
+@pytest.fixture
+def mock_csv_data():
+    """Fixture to provide mock CSV field permissions."""
+    return [
+        {
+            "operation": "update",
+            "table_name": "user",
+            "field_name": "gmail_email",
+            "view": "viewer",
+            "update": "moderator",
+            "create": GLOBAL_ADMIN,
+        },
+        {
+            "operation": "create",
+            "table_name": "user",
+            "field_name": "name",
+            "view": "viewer",
+            "update": "moderator",
+            "create": GLOBAL_ADMIN,
+        },
+    ]
+
+
+@pytest.mark.django_db
+@pytest.mark.load_user_data_required  # see load_user_data_required in conftest.py
+def test_is_admin():
+    """Test that is_admin returns True for an admin user."""
+    admin_user = SeedUser.get_user(garry_name)
+
+    assert AccessControl.is_admin(admin_user) is True
+
+
+@pytest.mark.django_db
+@pytest.mark.load_user_data_required  # see load_user_data_required in conftest.py
+def test_is_not_admin():
+    """Test that is_admin returns True for an admin user."""
+    admin_user = SeedUser.get_user(wanda_admin_project)
+    assert AccessControl.is_admin(admin_user) is False
+
+
+@pytest.mark.parametrize(  # noqa: PT006 PT007
+    "request_user_name, response_related_user_name, expected_permission_type",
+    (
+        # Wanda is an admin project for website, Wally is on the same project => ADMIN_PROJECT
+        (wanda_admin_project, wally_name, ADMIN_PROJECT),
+        # Wally is a project member for website, Wanda is on the same project => MEMBER_PROJECT
+        (wally_name, wanda_admin_project, MEMBER_PROJECT),
+        # Garry is both a project admin for website and a global admin => ADMIN_GLOBAL
+        (garry_name, wally_name, GLOBAL_ADMIN),
+        # Wally is a project member of website and Garry is a project lead on the same team
+        # => MEMBER_PROJECT
+        (wally_name, garry_name, MEMBER_PROJECT),
+        # Garry is a global admin.  Even though Patti is not assigned to same team => ADMIN_GLOBAL
+        (garry_name, patti_name, GLOBAL_ADMIN),
+        # Patti has no project in common with Garry => ""
+        (patti_name, wally_name, ""),
+        # Zani is part of two projects with different permission types
+        # Zani is a MEMBER_PROJECT for website, Wally is assigned same team => MEMBER_PROJECT
+        (zani_name, wally_name, MEMBER_PROJECT),
+        # Zani is a project admin for website, Wally is assigned same team => ADMIN_PROJECT
+        (zani_name, patti_name, ADMIN_PROJECT),
+    ),
+)
+@pytest.mark.django_db
+@pytest.mark.load_user_data_required  # see load_user_data_required in conftest.py
+def test_get_most_privileged_perm_type(
+    request_user_name, response_related_user_name, expected_permission_type
+):
+    """Test that the correct permission type is returned."""
+    request_user = SeedUser.get_user(request_user_name)
+    response_related_user = SeedUser.get_user(response_related_user_name)
+    assert (
+        AccessControl.get_most_privileged_perm_type(request_user, response_related_user)
+        == expected_permission_type
+    )
+
+
---
+++ app/core/tests/utils/load_data.py
@@ -0,0 +1,108 @@
+import copy
+
+from constants import GLOBAL_ADMIN
+from constants import ADMIN_PROJECT
+from constants import MEMBER_PROJECT
+from constants import PRACTICE_LEAD_PROJECT
+from core.models import Project
+from core.tests.utils.seed_constants import garry_name
+from core.tests.utils.seed_constants import patrick_practice_lead
+from core.tests.utils.seed_constants import patti_name
+from core.tests.utils.seed_constants import people_depot_project
+from core.tests.utils.seed_constants import valerie_name
+from core.tests.utils.seed_constants import wally_name
+from core.tests.utils.seed_constants import wanda_admin_project
+from core.tests.utils.seed_constants import website_project_name
+from core.tests.utils.seed_constants import winona_name
+from core.tests.utils.seed_constants import zani_name
+from core.tests.utils.seed_user import SeedUser
+
+
+def load_data():
+    """Populalates projects, users, and userpermissions with seed data
+    that is used by the tests in the core app.
+
+    Called from django_db_setup which is automatcallly called by pytest-django
+    before any test is executed.
+
+    Creates website_project and people_depot projects.  Populates users
+    as follows:
+    - Wanda is the project lead for the website project
+    - Wally and Winona are members of the website project
+    - Patti is a member of the People Depot project
+    - Patrick is the project lead for the People Depot project
+
+    - Garry is a global admin
+    - Zani is a member of the website project and the project lead for the People Depot project
+    - Valerie is a verified user with no UserPermission assignments.
+    """
+    projects = [website_project_name, people_depot_project]
+    for project_name in projects:
+        project = Project.objects.create(name=project_name)
+        project.save()
+    SeedUser.create_user(
+        first_name=wanda_admin_project, description="Website project admin"
+    )
+    SeedUser.create_user(first_name=wally_name, description="Website member")
+    SeedUser.create_user(first_name=winona_name, description="Website member")
+    SeedUser.create_user(
+        first_name=zani_name,
+        description="Website member and People Depot project admin",
+    )
+    SeedUser.create_user(first_name=patti_name, description="People Depot member")
+    SeedUser.create_user(
+        first_name=patrick_practice_lead, description="People Depot project admin"
+    )
+    SeedUser.create_user(first_name=garry_name, description="Global admin")
+    SeedUser.get_user(garry_name).save()
+    SeedUser.create_user(first_name=valerie_name, description="Verified user")
+
+    related_data = [
+        {"first_name": garry_name, "permission_type_name": GLOBAL_ADMIN},
+        {
+            "first_name": garry_name,
+            "project_name": website_project_name,
+            "permission_type_name": ADMIN_PROJECT,
+        },
+        {
+            "first_name": wanda_admin_project,
+            "project_name": website_project_name,
+            "permission_type_name": ADMIN_PROJECT,
+        },
+        {
+            "first_name": wally_name,
+            "project_name": website_project_name,
+            "permission_type_name": MEMBER_PROJECT,
+        },
+        {
+            "first_name": winona_name,
+            "project_name": website_project_name,
+            "permission_type_name": MEMBER_PROJECT,
+        },
+        {
+            "first_name": patti_name,
+            "project_name": people_depot_project,
+            "permission_type_name": MEMBER_PROJECT,
+        },
+        {
+            "first_name": patrick_practice_lead,
+            "project_name": people_depot_project,
+            "permission_type_name": PRACTICE_LEAD_PROJECT,
+        },
+        {
+            "first_name": zani_name,
+            "project_name": people_depot_project,
+            "permission_type_name": ADMIN_PROJECT,
+        },
+        {
+            "first_name": zani_name,
+            "project_name": website_project_name,
+            "permission_type_name": MEMBER_PROJECT,
+        },
+    ]
+
+    for data in related_data:
+        user = SeedUser.get_user(data["first_name"])
+        params = copy.deepcopy(data)
+        del params["first_name"]
+        SeedUser.create_related_data(user=user, **params)
