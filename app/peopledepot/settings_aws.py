e1f5fccfbf90c093fc0c7a7072d86e87e58286e0
drakeredwind01 <drakeredwind01@gmail.com>
2026-06-10 16:29:49 -0700

    solving, 672, Add gunicorn and Whitenoise, and set up Dockerfile-aws and entrypoint-aws.sh #672

---
+++ app/peopledepot/settings_aws.py
@@ -0,0 +1,25 @@
+from .settings import *  # noqa: F401, F403
+
+INSTALLED_APPS = [
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
+    # 3rd party
+    "django_extensions",
+    "rest_framework",
+    "phonenumber_field",
+    "timezone_field",
+    # Local
+    "core",
+    "data",
+]
+
+REST_FRAMEWORK = {
+    "DEFAULT_PERMISSION_CLASSES": ("core.api.permissions.DenyAny",),
+    "DEFAULT_AUTHENTICATION_CLASSES": (
+        "rest_framework_jwt.authentication.JSONWebTokenAuthentication",
+    ),
+}
