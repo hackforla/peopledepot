52bc133dd9fc2dfe22b21f70669150928b0cd7ed
drakeredwind01 <43461626+drakeredwind01@users.noreply.github.com>
2026-09-23 15:01:55 -0700

    Add gunicorn and Whitenoise, and set up Dockerfile-aws and entrypoint-aws.sh #672 (#692)

--- scripts/update-dependencies.sh
+++ scripts/update-dependencies.sh
@@ -5,3 +5,6 @@ set -x
 
 # generate requirements.txt with the latest package versions
 docker compose exec web uv pip compile -o requirements.txt requirements.in --no-header --upgrade
+
+# generate requirements-aws.txt with the latest package versions
+docker compose exec web uv pip compile -o requirements-aws.txt requirements-aws.in --no-header --upgrade --python-version 3.10
